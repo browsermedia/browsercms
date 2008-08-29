e5bbc83cc8757ef836183ceecd779aafdcd80b50
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2008-08-29 15:45:00 +0000

    Cleaned up syntax
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@2431 0d4fd071-7843-0410-978a-dc594407b538

--- spec/models/connector_spec.rb
+++ spec/models/connector_spec.rb
@@ -14,9 +14,7 @@ describe Connector do
   it "should not delete blocks when deleting a connector" do
     b = create_html_block
     c = create_connector(:content_block => b)
-    lambda do
-      c.destroy
-    end.should change(Connector, :count).by(-1)
+    lambda {c.destroy}.should change(Connector, :count).by(-1)
     f = HtmlBlock.find(b)
     f.should_not be_nil
   end
