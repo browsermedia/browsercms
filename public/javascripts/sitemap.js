2fd51c3f2e7b40453894dac3f36c530760b48243
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2008-08-31 20:05:47 +0000

    Added drap & drop, not hoooked up to AJAX yet though
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@2454 0d4fd071-7843-0410-978a-dc594407b538

--- public/javascripts/sitemap.js
+++ public/javascripts/sitemap.js
@@ -18,11 +18,28 @@ document.observe('dom:loaded', function() {
 
   //Now make all the list items draggable
   $$('#sitemap li').each(function(e){
-    new Draggable(e, {revert: true});    
+    new Draggable(e, {revert: true });    
+  });
+  
+  //Make all the section links and labels droppable
+  $$('#sitemap a.section, #sitemap span.section').each(function(e){
+    Droppables.add(e, {
+      hoverclass: 'drop-target',
+      onDrop: function(e) {
+        if(e != this.element.up() && !e.descendantOf(this.element.up())) {
+          parent = this.element.up();
+          list = parent.down('ul');
+          list.show();
+          parent.down('img').src = '/images/cms/icons/actions/folder_open.png';
+          list.appendChild(e);  
+          e.setStyle({'z-index': 0, top: 0, left: 0});          
+        }
+      }
+    });    
   });
   
   //Attach click handlers to the icons
-  $$('#sitemap a').each(function(e){
+  $$('#sitemap a.section').each(function(e){
     e.observe('click', function(event){ 
       parent = event.element().up().up();
       list = parent.down('ul');
