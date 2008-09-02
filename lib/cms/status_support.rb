6959ec7cdddb6aa81cda21c93828a38d80e9b97f
patrick <patrick@0d4fd071-7843-0410-978a-dc594407b538>
2008-09-02 20:37:07 +0000

    Forgot some files
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@2475 0d4fd071-7843-0410-978a-dc594407b538

---
+++ lib/cms/status_support.rb
@@ -0,0 +1,41 @@
+# Created by IntelliJ IDEA.
+# User: Patrick Peak
+# Date: Sep 2, 2008
+# Time: 3:13:06 PM
+# To change this template use File | Settings | File Templates.
+module Cms::StatusSupport
+
+  def self.included(base_class)
+      base_class.class_eval do
+        before_validation_on_create :set_default_status
+      end
+  end
+  
+  IN_PROGRESS = "IN_PROGRESS"
+  PUBLISHED = "PUBLISHED"
+  HIDDEN = "HIDDEN"
+  ARCHIVED = "ARCHIVED"
+
+  DEFAULT_STATUS = IN_PROGRESS
+
+  def set_default_status
+    self.status = DEFAULT_STATUS if status.blank?
+  end
+
+  { #Define the action methods for each status, like publish and publish!, which set the status and call save
+  PUBLISHED => :publish,
+      HIDDEN => :hide,
+      ARCHIVED => :archive
+  }.each do |status, method_name|
+    define_method method_name do
+      self.status = status
+      save
+    end
+    define_method "#{method_name}!" do
+      self.status = status
+      save!
+    end
+  end
+
+
+end
