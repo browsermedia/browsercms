0fb2a08ccf5d26822edb7f51b97c15bf433dea99
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2008-09-08 20:13:56 +0000

    Added cms_path method
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@2537 0d4fd071-7843-0410-978a-dc594407b538

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,53 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ApplicationHelper do
+
+  describe "path generation" do
+    before do
+      @block = new_html_block
+      @block.id = 7
+    end
+    {
+                                    "'blocks'" => "/cms/blocks",
+                                   "'/blocks'" => "/cms/blocks",
+                                     ":blocks" => "/cms/blocks",
+              ":blocks, :type => 'html_block'" => "/cms/blocks?type=html_block",
+             "'blocks', :type => 'html_block'" => "/cms/blocks?type=html_block",
+            "'/blocks', :type => 'html_block'" => "/cms/blocks?type=html_block",
+                              "'blocks', :new" => "/cms/blocks/new",
+                             "'/blocks', :new" => "/cms/blocks/new",
+                               ":blocks, :new" => "/cms/blocks/new",
+        ":blocks, :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
+       "'blocks', :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
+      "'/blocks', :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
+                                      "@block" => "/cms/html_blocks/show/7",
+                     "'html_blocks', :id => 7" => "/cms/html_blocks?id=7",
+                    "'/html_blocks', :id => 7" => "/cms/html_blocks?id=7",
+                      ":html_blocks, :id => 7" => "/cms/html_blocks?id=7",
+                            "'html_blocks', 7" => "/cms/html_blocks/7",
+                         "'/html_blocks', '7'" => "/cms/html_blocks/7",
+                               "@block, :edit" => "/cms/html_blocks/edit/7",
+              "'html_blocks', :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
+             "'/html_blocks', :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
+               ":html_blocks, :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
+                     "'html_blocks', :edit, 7" => "/cms/html_blocks/edit/7",
+                  "'/html_blocks', :edit, '7'" => "/cms/html_blocks/edit/7",
+                "@block, :edit, :foo => 'bar'" => "/cms/html_blocks/edit/7?foo=bar",
+                      ":blocks, :edit, @block" => "/cms/blocks/edit/7",
+       ":blocks, :edit, @block, :foo => 'bar'" => "/cms/blocks/edit/7?foo=bar",
+              "'somewhere', :msg => 'foo bar'" => "/cms/somewhere?msg=foo+bar"
+      #TODO: Some possible enhancements
+      #"'/foo?x=1', :x => 2, :y => 3" => "/cms/foo?x=2&y=3"
+       
+    }.each do |args, path|
+      it "cms_path(#{args}) should == '#{path}'" do
+        eval("helper.cms_path(#{args})").should == path
+      end
+    end
+    
+    it "should be able to generate fully qualified urls" do
+      helper.cms_url('/foo').should == 'http://test.host/cms/foo'
+    end
+  end 
+  
+end
