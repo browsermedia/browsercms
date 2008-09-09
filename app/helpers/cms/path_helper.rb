c292fc20aee38555c2ad9bf89c8f70750a0bad0c
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2008-09-09 15:24:33 +0000

    Converted portlet types view to use ERB and cms_path
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@2541 0d4fd071-7843-0410-978a-dc594407b538

--- app/helpers/cms/path_helper.rb
+++ app/helpers/cms/path_helper.rb
@@ -4,6 +4,9 @@ module Cms
     #If you pass a symbol, this method will assume that is the controller.
     #If you pass an object like an Active Record model, then it will construct the url for that model.
     #If the last argument is a Hash, it will append those values as querystring parameters
+    #If the action is :create_or_update, this will generate the proper url based on the status of the model.
+    #For example, if you call cms_path(@block, :create_or_update), if the block is a new record, it will return '/cms/blocks/create',
+    #but if the record is not a new record, if will return '/cms/blocks/update/1'
     def cms_path(*args)    
       paths = ["", "cms"]
       params = Hash === args.last ? args.pop : {}
@@ -13,8 +16,18 @@ module Cms
         paths << first.to_s.sub(/^\//,'')
       else
         paths << first.class.to_s.pluralize.underscore
-        paths << (args.delete_at(0) || "show")
-        paths << first.to_param
+        action = (args.delete_at(0) || "show")
+        if action.to_sym == :create_or_update
+          if first.new_record?
+            paths << :create
+          else
+            paths << :update
+            paths << first.to_param
+          end
+        else
+          paths << action
+          paths << first.to_param
+        end
       end
 
       paths += args.map(&:to_param)
@@ -30,5 +43,6 @@ module Cms
     def cms_url(*args)
       "#{request.protocol}#{request.host_with_port}#{cms_path(*args)}"
     end
+        
   end
 end
--- spec/helpers/path_helper_spec.rb
+++ spec/helpers/path_helper_spec.rb
@@ -4,39 +4,42 @@ describe Cms::PathHelper do
 
   describe "path generation" do
     before do
-      @block = new_html_block
-      @block.id = 7
+      @new_block = mock("html_block", :new_record? => true, :class => HtmlBlock)
+      @existing_block = mock("html_block", :new_record? => false, :to_param => 7, :class => HtmlBlock)      
     end
     {
-                                    "'blocks'" => "/cms/blocks",
-                                   "'/blocks'" => "/cms/blocks",
-                                     ":blocks" => "/cms/blocks",
-              ":blocks, :type => 'html_block'" => "/cms/blocks?type=html_block",
-             "'blocks', :type => 'html_block'" => "/cms/blocks?type=html_block",
-            "'/blocks', :type => 'html_block'" => "/cms/blocks?type=html_block",
-                              "'blocks', :new" => "/cms/blocks/new",
-                             "'/blocks', :new" => "/cms/blocks/new",
-                               ":blocks, :new" => "/cms/blocks/new",
-        ":blocks, :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
-       "'blocks', :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
-      "'/blocks', :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
-                                      "@block" => "/cms/html_blocks/show/7",
-                     "'html_blocks', :id => 7" => "/cms/html_blocks?id=7",
-                    "'/html_blocks', :id => 7" => "/cms/html_blocks?id=7",
-                      ":html_blocks, :id => 7" => "/cms/html_blocks?id=7",
-                            "'html_blocks', 7" => "/cms/html_blocks/7",
-                         "'/html_blocks', '7'" => "/cms/html_blocks/7",
-                               "@block, :edit" => "/cms/html_blocks/edit/7",
-              "'html_blocks', :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
-             "'/html_blocks', :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
-               ":html_blocks, :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
-                     "'html_blocks', :edit, 7" => "/cms/html_blocks/edit/7",
-                  "'/html_blocks', :edit, '7'" => "/cms/html_blocks/edit/7",
-                "@block, :edit, :foo => 'bar'" => "/cms/html_blocks/edit/7?foo=bar",
-                      ":blocks, :edit, @block" => "/cms/blocks/edit/7",
-       ":blocks, :edit, @block, :foo => 'bar'" => "/cms/blocks/edit/7?foo=bar",
-              "'somewhere', :msg => 'foo bar'" => "/cms/somewhere?msg=foo+bar",
-              "'somewhere', :block_id => @block" => "/cms/somewhere?block_id=7"
+                                      "'blocks'" => "/cms/blocks",
+                                     "'/blocks'" => "/cms/blocks",
+                                       ":blocks" => "/cms/blocks",
+                ":blocks, :type => 'html_block'" => "/cms/blocks?type=html_block",
+               "'blocks', :type => 'html_block'" => "/cms/blocks?type=html_block",
+              "'/blocks', :type => 'html_block'" => "/cms/blocks?type=html_block",
+                                "'blocks', :new" => "/cms/blocks/new",
+                               "'/blocks', :new" => "/cms/blocks/new",
+                                 ":blocks, :new" => "/cms/blocks/new",
+          ":blocks, :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
+         "'blocks', :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
+        "'/blocks', :new, :type => 'html_block'" => "/cms/blocks/new?type=html_block",
+                               "@existing_block" => "/cms/html_blocks/show/7",
+               ":blocks, :show, @existing_block" => "/cms/blocks/show/7",
+                       "'html_blocks', :id => 7" => "/cms/html_blocks?id=7",
+                      "'/html_blocks', :id => 7" => "/cms/html_blocks?id=7",
+                        ":html_blocks, :id => 7" => "/cms/html_blocks?id=7",
+                              "'html_blocks', 7" => "/cms/html_blocks/7",
+                           "'/html_blocks', '7'" => "/cms/html_blocks/7",
+                        "@existing_block, :edit" => "/cms/html_blocks/edit/7",
+                "'html_blocks', :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
+               "'/html_blocks', :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
+                 ":html_blocks, :edit, :id => 7" => "/cms/html_blocks/edit?id=7",
+                       "'html_blocks', :edit, 7" => "/cms/html_blocks/edit/7",
+                    "'/html_blocks', :edit, '7'" => "/cms/html_blocks/edit/7",
+         "@existing_block, :edit, :foo => 'bar'" => "/cms/html_blocks/edit/7?foo=bar",
+               ":blocks, :edit, @existing_block" => "/cms/blocks/edit/7",
+":blocks, :edit, @existing_block, :foo => 'bar'" => "/cms/blocks/edit/7?foo=bar",
+                 "@new_block, :create_or_update" => "/cms/html_blocks/create",
+            "@existing_block, :create_or_update" => "/cms/html_blocks/update/7",
+                "'somewhere', :msg => 'foo bar'" => "/cms/somewhere?msg=foo+bar",
+     "'somewhere', :block_id => @existing_block" => "/cms/somewhere?block_id=7"
       #TODO: Some possible enhancements
       #"'/foo?x=1', :x => 2, :y => 3" => "/cms/foo?x=2&y=3"
        
