67bc4d688dc51a19cb75fd8417c5124fdf0c54a6
patrick <patrick@0d4fd071-7843-0410-978a-dc594407b538>
2008-09-26 19:29:00 +0000

    Case 1496: Refactored how portlets use forms. Simplified PortletsController so it only handles the :new case.
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@2802 0d4fd071-7843-0410-978a-dc594407b538

--- app/controllers/cms/portlets_controller.rb
+++ app/controllers/cms/portlets_controller.rb
@@ -1,30 +1,12 @@
-class Cms::PortletsController < Cms::ResourceController
-  
-  layout 'cms/content_library'  
-  
+class Cms::PortletsController < Cms::BaseController
+
+  layout 'cms/content_library'
+
   def new
-    if params[:portlet_type_id]
-      @portlet_type = PortletType.find(params[:portlet_type_id])
-      @portlet = @portlet_type.portlets.build
-    else
-      render :template => 'cms/portlets/select_portlet_type'
-    end
+    @portlet_type = PortletType.find(params[:portlet_type_id])
+    @block = @portlet_type.portlets.build
+    @content_type = ContentType.find_by_key("portlet")
+    render :template => 'cms/blocks/new'
   end
-      
-  def resource_name
-    @portlet_type ? @portlet_type.underscore : controller_name
-  end
-  
-  def variable_name
-    "portlet"
-  end
-  
-  def index_url
-    cms_url :portlets
-  end
-  
-  def show_url
-    cms_url :portlets
-  end
-  
+
 end
