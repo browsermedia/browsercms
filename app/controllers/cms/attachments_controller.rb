a795f5f69e8042eb6a9096b3e640c35111b95ebd
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2008-11-07 19:33:29 +0000

    Case 1780, Case 1783: CMS User now see correct version of file/image
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@3172 0d4fd071-7843-0410-978a-dc594407b538

---
+++ app/controllers/cms/attachments_controller.rb
@@ -0,0 +1,11 @@
+class Cms::AttachmentsController < Cms::BaseController
+  def show
+    @attachment = Attachment.find(params[:id])
+    @attachment = @attachment.as_of_version(params[:version]) if params[:version]
+    send_data(@attachment.data, 
+      :filename => @attachment.name,
+      :type => @attachment.file_type,
+      :disposition => false
+    )     
+  end
+end
