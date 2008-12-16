c0be80ae3723c3b5e2b1758919c9f9c2e8aee0d4
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2008-12-16 14:15:53 +0000

    minor tweak to data loader
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@3592 0d4fd071-7843-0410-978a-dc594407b538

--- lib/cms/data_loader.rb
+++ lib/cms/data_loader.rb
@@ -1,8 +1,14 @@
 module Cms
   module DataLoader
     def method_missing(method_name, *args)
-      if method_name.to_s.match(/\Acreate_(.+)\Z/)
-        self.create($1, args[0], args[1] || {})
+      if md = method_name.to_s.match(/^create_(.+)$/)
+        begin
+          #Make sure this is an active record class
+          super unless md[1].classify.constantize.ancestors.include?(ActiveRecord::Base)
+        rescue NameError => e
+          super
+        end
+        self.create(md[1], args[0], args[1] || {})
       elsif @data && @data.has_key?(method_name)
         record = @data[method_name][args.first]
         record ? record.class.find(record.id) : nil
