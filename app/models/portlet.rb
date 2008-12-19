72411cee1fed4e8ee4338058ac454f1368582bc5
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2008-12-19 19:17:49 +0000

    Case #1854, Portlet can now be connected to page
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@3669 0d4fd071-7843-0410-978a-dc594407b538

--- app/models/portlet.rb
+++ app/models/portlet.rb
@@ -2,7 +2,9 @@ class Portlet < ActiveRecord::Base
 
   validates_presence_of :name
 
-  attr_accessor :request, :response, :params, :session
+  #These are here simply to temporarily hold these values
+  #Makes it easy to pass them through the process of selecting a portlet type
+  attr_accessor :connect_to_page_id, :connect_to_container
   
   def self.inherited(subclass)
     super if defined? super
