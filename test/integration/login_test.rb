362da1c0c3784b4c752b1c5cfc9e2cddcd2b502d
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2009-01-12 22:23:29 +0000

    Added infrastructure for integration tests
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@3870 0d4fd071-7843-0410-978a-dc594407b538

---
+++ test/integration/login_test.rb
@@ -0,0 +1,14 @@
+require File.join(File.dirname(__FILE__) + '/../test_helper')
+
+class LoginTest < ActionController::IntegrationTest
+  fixtures :all
+  
+  def test_login
+    get "/cms/login", {}, {"User-Agent" => "Test"}
+    assert_response :success
+
+    post_via_redirect "/cms/login", {:login => "cmsadmin", :password => "cmsadmin"}, {"User-Agent" => "Test"}
+    assert_equal '/cms', path
+    assert_equal 'Logged in successfully', flash[:notice]
+  end
+end
