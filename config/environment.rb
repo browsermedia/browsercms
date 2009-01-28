bb72d069365531eb6159a64874e2c810bfbf1e7a
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2009-01-28 20:30:41 +0000

    Fixed default time zone
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@4059 0d4fd071-7843-0410-978a-dc594407b538

--- config/environment.rb
+++ config/environment.rb
@@ -42,7 +42,7 @@ Rails::Initializer.run do |config|
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
-  config.time_zone = 'Eastern Time (US & Canada)'
+  config.active_record.default_timezone = :local
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
@@ -65,6 +65,6 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
-  config.action_controller.cache_store = :file_store, "#{RAILS_ROOT}/tmp/cache"
+  #config.action_controller.cache_store = :file_store, "#{RAILS_ROOT}/tmp/cache"
   #config.active_record.observers = :content_observer
 end  
