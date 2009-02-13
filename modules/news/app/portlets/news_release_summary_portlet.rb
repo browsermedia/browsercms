8306d383136dd5363d962b20a577133d3df47556
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2009-02-13 20:37:30 +0000

    Fix to news release summary portlet
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@4356 0d4fd071-7843-0410-978a-dc594407b538

--- modules/news/app/portlets/news_release_summary_portlet.rb
+++ modules/news/app/portlets/news_release_summary_portlet.rb
@@ -3,7 +3,7 @@ class NewsReleaseSummaryPortlet < Portlet
   def renderer(portlet)
     lambda do
       locals = {}
-      if portlet.category_id
+      if !portlet.category_id.blank?
         locals[:category] = Category.find(portlet.category_id)
         locals[:releases] = NewsRelease.all(:conditions => ["category_id = ?", locals[:category]])
       else
