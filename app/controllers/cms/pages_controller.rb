7c3d113a98b82a1fa71e4a90ce1bdfc93e942536
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2009-03-18 14:21:00 +0000

    Move templates to match Rails 2.3
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@4643 0d4fd071-7843-0410-978a-dc594407b538

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -124,7 +124,7 @@ class Cms::PagesController < Cms::BaseController
     end
     
     def load_templates
-      @templates = self.class.view_paths.map{|p| Dir["#{p}/templates/*.html.erb"]}.flatten.map{|f| File.basename(f, ".html.erb").titleize}.sort.uniq
+      @templates = self.class.view_paths.map{|p| Dir["#{p}/layouts/templates/*.html.erb"]}.flatten.map{|f| File.basename(f, ".html.erb").titleize}.sort.uniq
     end
     
 end
