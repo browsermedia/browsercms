9a2778b5b2d8f539e55060c226e93c59ad5cab2e
pbarry <pbarry@0d4fd071-7843-0410-978a-dc594407b538>
2009-03-19 19:20:00 +0000

    Added text are to CMS Form API
    git-svn-id: https://svn.browsermedia.com/bmedia/browsercms/trunk@4659 0d4fd071-7843-0410-978a-dc594407b538

--- app/helpers/cms/form_builder.rb
+++ app/helpers/cms/form_builder.rb
@@ -27,18 +27,23 @@ class Cms::FormBuilder < ActionView::Helpers::FormBuilder
   # These are the higher-level fields, 
   # that get wrapped in divs with labels, instructions, etc.
   
+  %w[text_area text_field].each do |f|
+    src = <<-end_src
+      def cms_#{f}(method, options={})
+        add_tabindex!(options)
+        cms_options = options.extract!(:label, :instructions)
+        render_cms_form_partial :#{f},
+          :method => method, :options => options, :cms_options => cms_options
+      end
+    end_src
+    class_eval src, __FILE__, __LINE__
+  end
+    
   def cms_tag_list(options={})
     add_tabindex!(options)
     cms_options = options.extract!(:label, :instructions)
-    render_cms_form_partial :tag_list, 
-      :options => options, :cms_options => cms_options
-  end
-  
-  def cms_text_field(method, options={})
-    add_tabindex!(options)
-    cms_options = options.extract!(:label, :instructions)
     render_cms_form_partial :text_field, 
-      :method => method, :options => options, :cms_options => cms_options
+      :options => options, :cms_options => cms_options
   end
 
   def cms_text_editor(method, options = {})
