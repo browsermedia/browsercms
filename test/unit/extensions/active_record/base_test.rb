8c03e54800205ec4f268671a5c36098dd5757edd
Cyrille Bonnet <cbonnet99@gmail.com>
2009-04-07 10:34:58 +1200

    Used Time.zone for updated_at to ensure that it works in all time zones

--- test/unit/extensions/active_record/base_test.rb
+++ test/unit/extensions/active_record/base_test.rb
@@ -4,7 +4,7 @@ class ActiveRecord::BaseTest < ActiveSupport::TestCase
   def test_updated_on_string
     base = HtmlBlock.new
     assert_nil base.updated_on_string
-    base.updated_at = Time.parse("1978-07-06")
+    base.updated_at = Time.zone.parse("1978-07-06")
     assert_equal "Jul 6, 1978", base.updated_on_string
   end
 end
