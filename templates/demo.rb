b231caa7e703c158074732dbf3052197f2bfce85
ssoper <sean.soper@gmail.com>
2009-04-30 14:17:28 -0400

    adding page_cache_directory settings to template for environments/production.rb

--- templates/demo.rb
+++ templates/demo.rb
@@ -7,5 +7,6 @@ environment 'SITE_DOMAIN="localhost:3000"', :env => "development"
 environment 'SITE_DOMAIN="localhost:3000"', :env => "test"
 environment 'SITE_DOMAIN="localhost:3000"', :env => "production"
 environment 'config.action_view.cache_template_loading = false', :env => "production"
+environment 'config.action_controller.page_cache_directory = RAILS_ROOT + "/public/cache/"', :env => "production"
 generate :browser_cms_demo_site
 rake "db:migrate"
