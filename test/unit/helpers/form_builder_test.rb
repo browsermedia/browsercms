5a664d4d34e69eb6087abf78f3b3dd9a21c7f4ae
peakpg <peakpg@gmail.com>
2009-04-30 16:12:43 -0400

    Fixed problem with instructions on textfields. Added some tests for portlet behavior.

---
+++ test/unit/helpers/form_builder_test.rb
@@ -0,0 +1,17 @@
+require File.join(File.dirname(__FILE__), '../../test_helper')
+
+class FormBuilderTest < ActionView::TestCase
+
+  def setup
+
+  end
+
+  def teardown
+
+  end
+
+
+  test "Text field includes instructions" do
+    
+  end
+end
--- test/unit/models/portlet_test.rb
+++ test/unit/models/portlet_test.rb
@@ -7,5 +7,15 @@ class PortletTest < ActiveSupport::TestCase
     assert_equal "FOO", Portlet.find(portlet.id).foo
     assert_equal "Dynamic Portlet", portlet.portlet_type_name
   end
-  
+
+  def test_portlets_consistently_load_the_same_number_of_types
+    
+    list = Portlet.types
+    assert list.size > 0
+
+    DynamicPortlet.create!(:name=>"test 1")
+    DynamicPortlet.create!(:name=>"test 2")
+
+    assert_equal list.size, Portlet.types.size
+  end
 end
