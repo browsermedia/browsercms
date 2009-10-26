ca81344b63308e1b0a2245cd18e278807941e2a4
jdettmann <jdettmann@browsermedia.com>
2009-10-26 16:25:30 -0400

    Fix vendored gem routes bug by Jon Leighton LIGHTHOUSE #138

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ if RAILS_ENV == "test"
   end
 end      
 
-ActionController::Routing::Routes.draw do |map|
-  map.connect "/__test__", :controller => "cms/content", :action => "show_page_route"
-  map.routes_for_browser_cms
+# Only load these routes if this is the "root" application
+if RAILS_ROOT == File.expand_path(File.dirname(__FILE__) + "/..")
+  ActionController::Routing::Routes.draw do |map|
+    map.connect "/__test__", :controller => "cms/content", :action => "show_page_route"
+    map.routes_for_browser_cms
+  end
 end
