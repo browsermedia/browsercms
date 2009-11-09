fe555a9582f7f5d948b4dce3bce499ebc5214b68
ssoper <sean.soper@gmail.com>
2009-11-09 14:29:37 -0500

    adding portlets to have your password mailed to you if you forget it and to reset it. tests included

---
+++ db/migrate/20091109175123_add_reset_token_to_users.rb
@@ -0,0 +1,9 @@
+class AddResetTokenToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :reset_token, :string
+  end
+
+  def self.down
+    remove_column :users, :reset_token
+  end
+end
