e7e0849c61901cea888a9edccb024822b0cbcf08
peakpg <peakpg@gmail.com>
2010-01-22 18:01:32 -0500

    Fixed Dashboard under SQL lite [#208]

--- test/functional/cms/dashboard_controller_test.rb
+++ test/functional/cms/dashboard_controller_test.rb
@@ -13,5 +13,12 @@ class Cms::DashboardControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Dashboard"
   end
-  
+
+  # This is a bit of a hack. Ideally would like the entire
+  # test suite to run against both MySQL and SQLite
+  # But this was the only SQLite test to fail.
+  def test_index_with_sqlite
+    ActiveRecord::Base.establish_connection(:test_sqlite3)
+    test_index
+  end
 end
