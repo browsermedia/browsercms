7d985b3aeb3ab1e9aa44598ed993a675c4c71e8c
Jeffrey Dettmann <jdettmann@jdettmann.(none)>
2010-05-14 17:13:15 -0400

    Change affects *ness content types so make sure is single; bump version to 3.2

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,9 +2,10 @@
 
 # Add new inflection rules using the following format 
 # (all these examples are active by default):
-# Inflector.inflections do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
+ActiveSupport::Inflector.inflections do |inflect|
+  # inflect.plural /^(ox)$/i, '\1en'
+  # inflect.singular /^(ox)en/i, '\1'
+  # inflect.irregular 'person', 'people'
+  # inflect.uncountable %w( fish sheep )
+  inflect.singular /^(.*ness)$/i, '\1'
+end
