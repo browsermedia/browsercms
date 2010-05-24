3968e728571fa4fdd5cb61da6c419e05a5a726c0
James Prior <jamesprior@midir.local>
2010-05-24 09:56:19 -0400

    Updating init for new gemspec

--- lib/cms/init.rb
+++ lib/cms/init.rb
@@ -4,7 +4,7 @@ module Cms
 
     define_method(:root) { __root__ }
 
-    ::SPEC = eval(File.read(__root__ + '/browsercms.gemspec'))
+    ::SPEC = eval(File.read(__root__ + '/browsercms-namespaced.gemspec'))
 
     attr_accessor :attachment_file_permission
 
