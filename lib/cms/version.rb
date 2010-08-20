30dc5f3a605a26111db7ad1ed0902a3e1e47a28c
peakpg <peakpg@gmail.com>
2010-08-20 15:59:52 -0400

    Rewrote the router to use new Rails 3. Still some broken routes to debug

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -1,5 +1,5 @@
 ##
 # Allows the precise version of BrowserCMS to be determined programmatically.
 module Cms
-  VERSION = "3.1.0"
+  VERSION = "3.5.0"
 end
--- test/unit/models/page_route_test.rb
+++ test/unit/models/page_route_test.rb
@@ -25,5 +25,6 @@ class PageRouteTest < ActiveSupport::TestCase
       }
     }, route.options_map)
   end
-  
+
+
 end
