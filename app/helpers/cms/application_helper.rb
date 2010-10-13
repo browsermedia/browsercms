cda9d02af0bbaecdfb32d46e9e9b039e06be79a3
peakpg <peakpg@gmail.com>
2010-10-13 14:59:27 -0400

    Fixed issue where javascript and stylesheet tests were failing due to changes in to_a

--- app/helpers/cms/application_helper.rb
+++ app/helpers/cms/application_helper.rb
@@ -4,7 +4,7 @@ module Cms
 
     def require_stylesheet_link(sources=(), content_area=:html_head)
       @required_stylesheet_links ||= []
-      new_links = sources.to_a - @required_stylesheet_links
+      new_links = Array(sources) - @required_stylesheet_links
       if !new_links.empty?
         @required_stylesheet_links |= new_links
         content_for(content_area, stylesheet_link_tag(new_links))
@@ -13,7 +13,7 @@ module Cms
 
     def require_javascript_include(sources=(), content_area=:html_head)
       @required_javascript_includes ||= []
-      new_links = sources.to_a - @required_javascript_includes
+      new_links = Array(sources) - @required_javascript_includes
       if !new_links.empty?
         @required_javascript_includes |= new_links
         content_for(content_area, javascript_include_tag(new_links))
