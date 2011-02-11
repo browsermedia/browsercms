ebf51eaba35efaad11379a2a247ddb76d37227a7
peakpg <peakpg@gmail.com>
2011-02-11 18:04:43 -0500

    Cleaned up comments. Installation script now works.

--- lib/generators/cms/install_module/install_module_generator.rb
+++ lib/generators/cms/install_module/install_module_generator.rb
@@ -1,29 +1,20 @@
 require 'generators/browser_cms/cms/cms_generator'
 
 module Cms
+
+  # A generic generator for installing BrowserCMS modules into a project. This will do the following:
+  #
+  # 1. Add the Gem to your Gemfile
+  # 2. Call the name_of_module:install generator for that gem.
+  # 3. Add the routes to the project
   class InstallModuleGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
-    include BrowserCms::Generators::GemFileExtractor
-    # To install a module, i must
-
-    # require the gem (so that it adds its own migrations to loadpath)
-
-    def add_gem
+    def add_and_install_module
       gem name
-
-      generator('browser_cms:cms')
+      generate("#{name}:install")
+      route "routes_for_#{name}"
     end
-#    def require_gem
-#      puts "Requiring #{name}"
-#
-#      # This doesn't work, because we don't have it in our project yet.
-#      require name
-#    end
 
-    def copy_files_from_gem
-      puts "Copy files"
-    end
-    # Call the copy files generator from core CMS generator.
   end
 end
