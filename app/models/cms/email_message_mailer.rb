96877cafeccd6d76b4489b85eb7e35897d98e65c
peakpg <peakpg@gmail.com>
2011-08-24 21:32:04 -0400

    [Rails 3.1] Fix Mailer deprecation warnings
    Upgrade Mailer so it using Rails 3.x ActionMailer API which removes warnings for charset.

--- app/models/cms/email_message_mailer.rb
+++ app/models/cms/email_message_mailer.rb
@@ -1,12 +1,11 @@
 module Cms
   class EmailMessageMailer < ActionMailer::Base
 
+
     def email_message(message)
-      @recipients = message.recipients
-      @from = message.sender
-      @subject = message.subject
-      @body = message.body
-      @content_type = message.content_type if message.content_type
+      email = {:to=>message.recipients, :from=>message.sender, :subject => message.subject, :body =>message.body}
+      email[:content_type]  = message.content_type if message.content_type
+      mail email
     end
 
   end
