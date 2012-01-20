2a035837be6fe7318e7df8b21d19f6ff66c198d8
Patrick Peak <peakpg@gmail.com>
2012-01-20 13:19:29 -0500

    [Performance Fix #12] Optimize empty folder icon
    * Use the existing children to determine if the section should get an open/closed icon.

--- app/helpers/cms/section_nodes_helper.rb
+++ app/helpers/cms/section_nodes_helper.rb
@@ -1,14 +1,14 @@
 module Cms
   module SectionNodesHelper
 
-    def section_icons(node)
+    def section_icons(section_node, children=[])
       folder_style = ""
       expander_image = "expand.png"
-      if top_level_section?(node)
+      if top_level_section?(section_node)
         folder_style = " large"
         expander_image = "gray_expand.png"
       end
-      if  node.child_nodes.empty?
+      if children.empty?
         image_tag("cms/sitemap/no_contents.png", :class => "no_folder_toggle#{folder_style}")
       else
         image_tag("cms/sitemap/#{expander_image}", :class => "folder_toggle#{folder_style}")
@@ -17,7 +17,7 @@ module Cms
 
     private
     def top_level_section?(node)
-      node.node.depth <= 2
+      node.depth <= 2
     end
 
   end
