97d7281cdadfd5893126a9dddc8e0b904251a03f
Patrick Peak <peakpg@gmail.com>
2012-02-16 16:40:36 -0500

    * Improved behavior for automatically launching a failed scenario.

--- features/support/debugging.rb
+++ features/support/debugging.rb
@@ -1,28 +1,5 @@
 require 'cms/data_loader'
 
-
-module LaunchBrokenTests
-  LAUNCH = false
-
-  def launch_broken_tests
-    LAUNCH
-  end
-end
-World(LaunchBrokenTests)
-
-After do |scenario|
-  if scenario.failed? && launch_broken_tests
-    save_and_open_page
-  end
-end
-
-AfterConfiguration do |config|
-  if config.formats[0].include?("Debug::Formatter")
-    LaunchBrokenTests::LAUNCH = true
-  end
-end
-
-
 require File.expand_path(File.join(__FILE__, "..", "..", "..", "test", "mock_file"))
 module FileOperations
 
---
+++ features/support/open_on_first_failure.rb
@@ -0,0 +1,25 @@
+# The first scenario that fails, we want to save and launch the page in the Browser.
+# We don't want to to open subsequent failures, as that can be HIGHLY annoying when running from the command line.
+#
+# Also, this https://gist.github.com/398643
+# has good info on how to save Assets/CSS so we can see the full version of the page.
+#
+module LaunchOnFirstFailure
+  class << self
+    attr_accessor :failed_tests
+    def failure_occurred
+      self.failed_tests = 0 unless failed_tests
+      self.failed_tests += 1
+    end
+    def failed_tests?
+      failed_tests && failed_tests >= 0
+    end
+  end
+end
+
+After do |scenario|
+  if scenario.failed? && !LaunchOnFirstFailure.failed_tests?
+    LaunchOnFirstFailure.failure_occurred
+    save_and_open_page
+  end
+end
