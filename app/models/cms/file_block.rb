940ca9f6786d4145cb94029845b89ca21f3b413f
Patrick Peak <peakpg@gmail.com>
2012-03-29 11:14:22 -0400

    * Make Image/File blocks use has_attachment

--- app/models/cms/file_block.rb
+++ app/models/cms/file_block.rb
@@ -1,7 +1,9 @@
 module Cms
   class FileBlock < Cms::AbstractFileBlock
 
-    acts_as_content_block :belongs_to_attachment => true, :taggable => true
+    acts_as_content_block :has_attachments => true, :taggable => true
+    has_attachment :file, :url => ":attachment_file_path"
+    validates_attachment_presence :file, :message => "You must upload a file"
 
     def self.display_name
       "File"
