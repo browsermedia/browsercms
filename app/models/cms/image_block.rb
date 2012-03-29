dd1421889c13377fb497c5572eabec6c9f43293d
Patrick Peak <peakpg@gmail.com>
2012-03-29 11:14:22 -0400

    * Make Image/File blocks use has_attachment

--- app/models/cms/image_block.rb
+++ app/models/cms/image_block.rb
@@ -1,7 +1,8 @@
 module Cms
   class ImageBlock < Cms::AbstractFileBlock
 
-    acts_as_content_block :belongs_to_attachment => true, :taggable => true
+    acts_as_content_block :has_attachments => true, :taggable => true
+    has_attachment :image, :url => ":attachment_file_path", :styles => {:thumb => "80x80"}
 
     def self.display_name
       "Image"
