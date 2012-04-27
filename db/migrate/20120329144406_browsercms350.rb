e202d4f7ee3ab3b12fb9adda10401c3712dd1a5e
Patrick Peak <peakpg@gmail.com>
2012-04-27 17:08:37 -0400

    First pass at migrating attachments directory.
    * Works if you request the live path of attachments
    * Does not work for attachment versions yet.
    * Need to condense migrations into a single 3_5_5 migration
    * Need to make other types of attachments work as well (custom blocks)

--- db/migrate/20120329144406_browsercms350.rb
+++ db/migrate/20120329144406_browsercms350.rb
@@ -1,5 +1,15 @@
 class Browsercms350 < ActiveRecord::Migration
   def change
+    update_attachment_schema
+
+
+  end
+
+
+
+
+  private
+  def update_attachment_schema
     rename_table :attachments, :cms_attachments if table_exists?(:attachments)
     rename_table :attachment_versions, :cms_attachment_versions if table_exists?(:attachment_versions)
 
@@ -7,8 +17,8 @@ class Browsercms350 < ActiveRecord::Migration
     rename_content_column :cms_attachments, :file_size, :data_file_size
     rename_content_column :cms_attachments, :file_type, :data_content_type
     rename_content_column :cms_attachments, :name, :data_file_name
-
-
+    # No longer used, calculated dynamically from data_file_name
+    remove_content_column :cms_attachments, :file_extension
     add_content_column :cms_attachments, :data_fingerprint, :string
     add_content_column :cms_attachments, :attachable_type, :string
     add_content_column :cms_attachments, :attachment_name, :string
--- lib/cms/upgrades/v3_5_0.rb
+++ lib/cms/upgrades/v3_5_0.rb
@@ -5,6 +5,33 @@ module Cms
       def upgrade_to_3_5_0
         gsub_file "config/environments/production.rb", /config.action_controller.page_cache_directory.+$/, ""
       end
+
+      module FileStorageUpdates
+
+        # Old paths are:
+        #       uploads/:year/:month/:day/:fingerprint
+        # i.e.  uploads/2012/04/27/fb598......
+        #
+        # New paths use paperclip's :id_partition which creates paths like:
+        #       uploads/000/000/001/:fingerprint
+        # where it splits the id into multiple directories
+        #
+        def new_file_location(attachment)
+          new_id_dir = "#{Paperclip::Interpolations.id_partition(AttachmentWrapper.new(attachment), nil)}/original"
+          return "#{new_id_dir}/#{attachment.data_fingerprint}", new_id_dir
+        end
+
+        # Allows us to use the Paperclip interpolations logic directly
+        class AttachmentWrapper
+          def initialize(attachment)
+            @attachment = attachment
+          end
+
+          def instance
+            @attachment
+          end
+        end
+      end
     end
   end
 end
