1e6965c41134e308d9ab2abb2b12ca36f539b3bd
Patrick Peak <peakpg@gmail.com>
2012-05-07 11:58:39 -0400

    Migrates old File/Image blocks
    * Old attachments are copied to new location (for versions as well)
    * Data is updated to map to old schema.
    * Needs refactoring
    * Doesn't work for custom attachments yet.

--- db/migrate/20120427193220_migrate_files.rb
+++ db/migrate/20120427193220_migrate_files.rb
@@ -5,17 +5,33 @@ class MigrateFiles < ActiveRecord::Migration
   include Cms::Upgrades::V3_5_0::FileStorageUpdates
 
   def up
-    Cms::Attachment.unscoped.where("attachable_type is NOT NULL").limit(10).each do |attachment|
+
+    # Move attachments
+    Cms::Attachment.unscoped.where("attachable_type is NOT NULL").each do |attachment|
       old_location = File.join(Cms::Attachment.configuration.attachments_root, attachment.file_location)
       new_file_location, new_dir = new_file_location(attachment)
       new_location = File.join(Cms::Attachment.configuration.attachments_root, new_file_location)
-      new_dir_path =  File.join(Cms::Attachment.configuration.attachments_root, new_dir)
+      new_dir_path = File.join(Cms::Attachment.configuration.attachments_root, new_dir)
 
-      FileUtils.mkdir_p(new_dir_path, :verbose=>true)
-      FileUtils.cp(old_location, new_location, :verbose=>true)
+      FileUtils.mkdir_p(new_dir_path, :verbose => true)
+      FileUtils.cp(old_location, new_location, :verbose => true)
 
       new_fingerprint = attachment.file_location.split("/").last
-      Cms::Attachment.unscoped.update_all({:data_fingerprint => new_fingerprint}, :id=>attachment.id)
+      Cms::Attachment.unscoped.update_all({:data_fingerprint => new_fingerprint}, :id => attachment.id)
+    end
+
+    # Move Attachment versions
+    Cms::Attachment::Version.unscoped.where("attachable_type is NOT NULL").each do |attachment|
+      old_location = File.join(Cms::Attachment.configuration.attachments_root, attachment.file_location)
+      new_file_location, new_dir = new_file_location(attachment)
+      new_location = File.join(Cms::Attachment.configuration.attachments_root, new_file_location)
+      new_dir_path = File.join(Cms::Attachment.configuration.attachments_root, new_dir)
+
+      FileUtils.mkdir_p(new_dir_path, :verbose => true)
+      FileUtils.cp(old_location, new_location, :verbose => true)
+
+      new_fingerprint = attachment.file_location.split("/").last
+      Cms::Attachment::Version.unscoped.update_all({:data_fingerprint => new_fingerprint}, :id => attachment.id)
     end
   end
 
