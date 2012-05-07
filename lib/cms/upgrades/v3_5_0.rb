862c5434ef8c59c2c4733d4bcf0a825febc0fbb5
Patrick Peak <peakpg@gmail.com>
2012-05-07 17:02:54 -0400

    Handle custom blocks with attachments
    * Update the 3_5_0 migrations to handle custom attachments
    * Can create migrations to 'fix' data for attachments

--- lib/cms/upgrades/v3_5_0.rb
+++ lib/cms/upgrades/v3_5_0.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module Cms
   module Upgrades
 
@@ -6,6 +8,7 @@ module Cms
         gsub_file "config/environments/production.rb", /config.action_controller.page_cache_directory.+$/, ""
       end
 
+      # Add additional methods to ActiveRecord::Migration when this file is required.
       module FileStorageUpdates
 
         # Old paths are:
@@ -21,6 +24,46 @@ module Cms
           return "#{new_id_dir}/#{attachment.data_fingerprint}", new_id_dir
         end
 
+
+        # For a given class with an Attachment association (pre-3.5.0), migrate its attachment data to match the new
+        # table structure.
+        def migrate_attachment_for(klass)
+          klass.unscoped.find_each do |block|
+            Cms::Attachment.unscoped.update_all({:attachable_id => block.id,
+                                                 :attachable_version => block.version,
+                                                 :attachable_type => klass.name,
+                                                 :attachment_name => "file",
+                                                 :cardinality => 'single'},
+                                                {:id => block.attachment_id})
+          end
+
+          attachable_type = klass.name
+          # Special handling for File/Image blocks
+          if klass == Cms::FileBlock
+            attachable_type = "Cms::AbstractFileBlock"
+          elsif klass == Cms::ImageBlock
+            # Only need to do this once for both Image and File blocks since they share a table.
+            return
+          end
+
+          migrated_attachment_for_versioned_table(klass, attachable_type)
+
+        end
+
+        def migrated_attachment_for_versioned_table(model_class, attachable_type)
+          version_model = model_class.version_class
+
+          found = version_model.find_by_sql("SELECT original_record_id, attachment_id, version from #{version_model.table_name}")
+          found.each do |version_record|
+            Cms::Attachment::Version.unscoped.update_all({:attachable_id => version_record.original_record_id,
+                                                          :attachable_version => version_record.version,
+                                                          :attachable_type => attachable_type,
+                                                          :attachment_name => "file",
+                                                          :cardinality => 'single'},
+                                                         {:original_record_id => version_record.attachment_id})
+          end
+        end
+
         # Move the attachment files from the old file path path to new one. Updates the Attachment record to match.
         def move_attachments_to_new_location
           migrate_attachments_file_location(Cms::Attachment)
@@ -33,7 +76,7 @@ module Cms
         # a file exists before moving. Failure to move isn't necessary an error, since it may have been moved in
         # an earlier version
         def migrate_attachments_file_location(model_class)
-          model_class.unscoped.where("attachable_type is NOT NULL").each do |attachment|
+          model_class.unscoped.each do |attachment|
             old_location = File.join(Cms::Attachment.configuration.attachments_root, attachment.file_location)
             new_file_location, new_dir = new_file_location(attachment)
             new_location = File.join(Cms::Attachment.configuration.attachments_root, new_file_location)
@@ -65,3 +108,4 @@ module Cms
     end
   end
 end
+ActiveRecord::Migration.send(:include, Cms::Upgrades::V3_5_0::FileStorageUpdates) if defined?(ActiveRecord::Migration)
