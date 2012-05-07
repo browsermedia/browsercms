0e31488bfb8a15f0d78d1e72bb86cf9d2b5bb683
Patrick Peak <peakpg@gmail.com>
2012-05-07 13:50:30 -0400

    Move details out of migration into class
    * Migrations don't need the details.

--- lib/cms/upgrades/v3_5_0.rb
+++ lib/cms/upgrades/v3_5_0.rb
@@ -21,6 +21,30 @@ module Cms
           return "#{new_id_dir}/#{attachment.data_fingerprint}", new_id_dir
         end
 
+        # Move the attachment files from the old file path path to new one. Updates the Attachment record to match.
+        def move_attachments_to_new_location
+          [Cms::Attachment, Cms::Attachment::Version].each do |model_class|
+            migrate_attachments_file_location(model_class)
+          end
+        end
+
+        private
+
+        def migrate_attachments_file_location(model_class)
+          model_class.unscoped.where("attachable_type is NOT NULL").each do |attachment|
+            old_location = File.join(Cms::Attachment.configuration.attachments_root, attachment.file_location)
+            new_file_location, new_dir = new_file_location(attachment)
+            new_location = File.join(Cms::Attachment.configuration.attachments_root, new_file_location)
+            new_dir_path = File.join(Cms::Attachment.configuration.attachments_root, new_dir)
+
+            FileUtils.mkdir_p(new_dir_path, :verbose => true)
+            FileUtils.cp(old_location, new_location, :verbose => true)
+
+            new_fingerprint = attachment.file_location.split("/").last
+            model_class.unscoped.update_all({:data_fingerprint => new_fingerprint}, :id => attachment.id)
+          end
+        end
+
         # Allows us to use the Paperclip interpolations logic directly
         class AttachmentWrapper
           def initialize(attachment)
