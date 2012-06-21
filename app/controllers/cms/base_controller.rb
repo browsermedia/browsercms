591debfb284ad5bb410598ccd4bfe323362821e7
Patrick Peak <peakpg@gmail.com>
2012-06-21 09:59:27 -0400

    * Rework all page, section, links to use button bar

--- app/controllers/cms/base_controller.rb
+++ app/controllers/cms/base_controller.rb
@@ -1,15 +1,11 @@
 module Cms
-class BaseController < Cms::ApplicationController
-  
-  before_filter :redirect_to_cms_site
-  before_filter :login_required
-  before_filter :cms_access_required
+  class BaseController < Cms::ApplicationController
 
-  layout 'cms/application'
-    
-#  verify :method => :post, :only => [:create]
-#  verify :method => :put, :only => [:update]
-#  verify :method => :delete, :only => [:destroy]
-    
-end
+    before_filter :redirect_to_cms_site
+    before_filter :login_required
+    before_filter :cms_access_required
+
+    layout 'cms/application'
+
+  end
 end
