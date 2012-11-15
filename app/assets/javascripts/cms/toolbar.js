de89dcc050ace4a5f114a0026967a4e3cb71e3f5
Patrick Peak <peakpg@gmail.com>
2012-11-15 16:42:34 -0500

    Can remove blocks from In Context pages via toolbar button.

--- app/assets/javascripts/cms/toolbar.js
+++ app/assets/javascripts/cms/toolbar.js
@@ -18,32 +18,50 @@ $(function () {
 jQuery(function ($) {
 
     $.cms_ajax = {
-
-        // Invoke a Rails aware (w/ CSRF token) PUT request.
-        put: function (path, success) {
+        // Add the CSRF token to an AJAX/JSON request.
+        setup:function () {
             $.ajaxSetup({
-                beforeSend: function (xhr) {
+                beforeSend:function (xhr) {
                     xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
                     xhr.setRequestHeader("Accept", "application/json");
                 }
             });
+        },
 
+        // Invoke a Rails aware (w/ CSRF token) PUT request.
+        put:function (path, success) {
+            $.cms_ajax.setup();
             $.ajax({
                 type:'POST',
                 url:path,
                 data:{ _method:'PUT'},
-                success: success
+                success:success
+            });
+
+        },
+        // Invoke a Rails aware (w/ CSRF token) DELETE request.
+        delete:function (path, success) {
+            $.cms_ajax.setup();
+            $.ajax({
+                type:'POST',
+                url:path,
+                data:{ _method:'DELETE'},
+                success:success
             });
 
         }
-    }
+    };
 
     $.cms_editor = {
         // Returns the widget that a user has currently selected.
         // @return [JQuery.Element]
-        selectedElement: function(){
+        selectedElement:function () {
             return $($('#mercury_iframe').contents()[0].activeElement);
+        },
+        // Most updates will need to reload the page. This function can be passed as a handler to ajax requests.
+        reload:function (data) {
+            window.location.reload();
         }
-    }
+    };
 });
 
