0481273ce3cc8f3ddccf1ef4c4cea5db815258af
Patrick Peak <peakpg@gmail.com>
2012-11-20 14:23:10 -0500

    Move blocks up/down/remove from connector view.

--- test/dummy/app/assets/javascripts/mercury.js
+++ test/dummy/app/assets/javascripts/mercury.js
@@ -321,15 +321,15 @@ window.Mercury = {
     behaviors: {
       //foreColor: function(selection, options) { selection.wrap('<span style="color:' + options.value.toHex() + '">', true) },
         moveBlockUp:   function(){
-            var move_up_path = $.cms_editor.selectedElement().data('move-up');
+            var move_up_path = $.cms_editor.selectedConnector().data('move-up');
             $.cms_ajax.put(move_up_path, $.cms_editor.save);
         },
         moveBlockDown: function(){
-            var move_down_path = $.cms_editor.selectedElement().data('move-down');
+            var move_down_path = $.cms_editor.selectedConnector().data('move-down');
             $.cms_ajax.put(move_down_path, $.cms_editor.save);
         },
         removeBlock: function(){
-            var path = $.cms_editor.selectedElement().data('remove');
+            var path = $.cms_editor.selectedConnector().data('remove');
             $.cms_ajax.delete(path, $.cms_editor.save);
         },
         htmlEditor: function() { Mercury.modal('/mercury/modals/htmleditor.html', { title: 'HTML Editor', fullHeight: true, handler: 'htmlEditor' }); }
