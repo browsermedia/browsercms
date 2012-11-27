32927e6b2c3e395cf346866f40a69c0ebcefe3cc
Patrick Peak <peakpg@gmail.com>
2012-11-27 16:49:24 -0500

    Editing a block will prompt users to save changes (if necessary)

--- app/controllers/cms/page_components_controller.rb
+++ app/controllers/cms/page_components_controller.rb
@@ -7,10 +7,6 @@ module Cms
       @page_component = PageComponent.new(params[:id], params[:content])
       if @page_component.save
         respond_with(@page_component)
-        # This does not work to prevent 204s from being returned after saving an unchanged resource.
-        #respond_with @page_component do |format|
-        #  format.json { render :json => @page_component, :status => :success, :location => @page_component }
-        #end
       else
         respond_with(@page_component.errors, :status => :unprocessable_entity)
       end
