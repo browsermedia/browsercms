7ac62b610df4879af78b41e0bb31cb5d967ac0c1
Patrick Peak <peakpg@gmail.com>
2013-02-21 16:46:05 -0500

    [#566] Ckeditor inline
    * Containers no longer display a box around them
    * Published and Draft status float to the right of the toolbar.

--- app/controllers/cms/inline_content_controller.rb
+++ app/controllers/cms/inline_content_controller.rb
@@ -7,11 +7,11 @@ module Cms
       content.update_attributes(params[:content])
       @page = Page.find_draft(params[:page_id])
       if (!@page.live?)
-        @page_status = "label label-important"
+        @page_status = "draft-status"
         @status_label = "DRAFT"
         @enable_publish = true
       else
-        @page_status = "label label-success"
+        @page_status = "published-status"
         @status_label = "Published"
         @enable_publish = false
       end
--- task_ckeditor.md
+++ task_ckeditor.md
@@ -1,2 +1,5 @@
 ## Issue #566: Add Ckeditor inline editing
 
+### Features
+
+* No need to toggle the editor on/off. Just click the area of the page you want to edit.
