ccc54d29a36b83306deb6ea2207c77b2b6899a0c
Patrick Peak <peakpg@gmail.com>
2013-09-17 17:25:58 -0400

    [#623] Use SimpleForm
    * Create :text_editor input 
    * Refactor html_blocks for both new and edit.

---
+++ app/inputs/text_editor_input.rb
@@ -0,0 +1,18 @@
+class TextEditorInput < SimpleForm::Inputs::TextInput
+
+  def input
+    tag_id = "#{object_name}_#{attribute_name}"
+    s = template.select_tag(:dhtml_selector,
+                                  template.options_for_select([["Rich Text", ""],["Simple Text", "disabled"]],
+                                  template.cookies[:editorEnabled] == 'true' ? "" : "disabled"),
+                                  :class => "#{object_name}_#{attribute_name}_dhtml_selector",
+                                  :onchange => "toggleEditor('#{tag_id}', this)".html_safe)
+    s += template.content_tag(:div, super, class: 'editor')
+
+  end
+
+  # Mark textarea with class for WYSIWYG editor.
+  def input_html_classes
+    super.push('editor')
+  end
+end
