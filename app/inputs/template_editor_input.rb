4b62280df20640eb483f0924a8a9c399d43f8ae8
Patrick Peak <peakpg@gmail.com>
2013-09-20 14:49:20 -0400

    [#623] Refactor portlets
    * Fix template_editor so it doesn't show label if a portlet has template editing disabled.

--- app/inputs/template_editor_input.rb
+++ app/inputs/template_editor_input.rb
@@ -2,16 +2,23 @@ class TemplateEditorInput < SimpleForm::Inputs::TextInput
 
   include Cms::Inputs::DefaultInput
 
+  def label
+    super if render_template_input?
+  end
+
   def input
-    if object.class.render_inline
+    if render_template_input?
       options[:default] = object.class.default_template
       options[:default_handler] = "erb" unless options[:default_handler]
       view = @builder.select "#{attribute_name}_handler", ActionView::Template.template_handler_extensions, selected: options[:default_handler]
-
       view << '<br />'.html_safe
-
       extract_default
       view << @builder.text_area(attribute_name, input_html_options).html_safe
     end
+  end
+
+  private
+  def render_template_input?
+    object.class.render_inline
   end
 end
