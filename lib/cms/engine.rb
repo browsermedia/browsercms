5a2de74d9fb0f690091625c1ef0e49d8a0f17c45
Patrick Peak <peakpg@gmail.com>
2013-09-24 17:33:10 -0400

    [#623] Configure SimpleForm
    * Engine handles correctly configuring simple_form as part of its bootup.

--- lib/cms/engine.rb
+++ lib/cms/engine.rb
@@ -29,13 +29,13 @@ module Cms
 
     config.cms = ActiveSupport::OrderedOptions.new
     config.cms.attachments = ActiveSupport::OrderedOptions.new
-    
+
     # Allows additional menu items to be added to the 'Tools' menu on the Admin tab.
     config.cms.tools_menu = ActiveSupport::OrderedOptions.new
 
     # Define configuration for the CKEditor
     config.cms.ckeditor = ActiveSupport::OrderedOptions.new
-    
+
     # Make sure we use our rails model template (rather then its default) when `rails g cms:content_block` is run.
     config.app_generators do |g|
       path = File::expand_path('../../templates', __FILE__)
@@ -71,13 +71,16 @@ module Cms
       # I.e.
       #   config.cms.site_domain = "www.browsercms.org"
       app.config.cms.site_domain = "localhost:3000"
-      
+
       # Determines which ckeditor file will be used to configure all instances.
       # There should be at most ONE of these, so use manifest files which require the below one to augement it.
       app.config.cms.ckeditor.configuration_file = 'bcms/ckeditor_standard_config.js'
-      
+
       # Define menu items to be added dynamically to the CMS Admin tab.
       app.config.cms.tools_menu = []
+
+      require 'cms/configure_simple_form'
+      require 'cms/configure_simple_form_bootstrap'
     end
 
     initializer 'browsercms.add_core_routes', :after => 'action_dispatch.prepare_dispatcher' do |app|
