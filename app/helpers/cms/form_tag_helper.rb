998904dd0d59bbf8663f950474c6f237a2f018f2
Patrick Peak <peakpg@gmail.com>
2013-09-24 14:41:21 -0400

    [#623] Remove old custom form inputs
    * Remove obsolete FormBuilder implementation

--- app/helpers/cms/form_tag_helper.rb
+++ app/helpers/cms/form_tag_helper.rb
@@ -24,40 +24,5 @@ module Cms
       end
     end
 
-    def content_requires_slug_field?(method, form_object)
-      method == :name && form_object.object.class.requires_slug?
-    end
-
-    def should_autogenerate_slug?(method, form_object)
-      content_requires_slug_field?(method, form_object) && form_object.object.new_record?
-    end
-
-    # A drop-down is just a specialized HTML select
-    
-    def drop_down_tag(name, option_tags = nil, options = {}) 
-      select_tag(name, option_tags, options)
-    end
-    
-    def drop_down(object, method, choices, options = {}, html_options = {})
-      select(object, method, choices, options, html_options)
-    end
-    
-    # A text editor is an HTML WYSIWYG editor.  The result of this
-    # will be a div with a select and a textarea in it.
-    
-    def text_editor_options(options={})
-      opts = options.dup
-      (opts[:class] ||= "") << " editor"
-      opts
-    end
-    
-    def text_editor_tag(name, content = nil, options = {})
-      text_area_tag(name, content, text_editor_options(options))
-    end
-    
-    def text_editor(object_name, method, options = {})
-      text_area(object_name, method, text_editor_options(options))
-    end
-        
   end
 end
--- app/inputs/attachments_input.rb
+++ app/inputs/attachments_input.rb
@@ -3,11 +3,11 @@ class AttachmentsInput < SimpleForm::Inputs::Base
   def input
     definitions = Cms::Attachment.definitions_for(object.class.name, :multiple)
     if definitions.empty?
-      template.render(partial: 'cms/form_builder/cms_no_attachments_defined', locals: {object: object})
+      template.render(partial: 'cms/attachments/no_attachments_defined', locals: {object: object})
     else
       names = definitions.keys.sort
       names.unshift "Select a type to upload a file" if names.size > 1
-      template.render(partial: 'cms/form_builder/cms_attachment_manager', locals: {:asset_definitions => definitions, :asset_types => names, f: @builder, object: object})
+      template.render(partial: 'cms/attachments/attachment_manager', locals: {:asset_definitions => definitions, :asset_types => names, f: @builder, object: object})
     end
   end
 
