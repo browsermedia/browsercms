af534aa39bc74720d1eaafd3abbc9ec092f48592
Andy Olliver <andy@artirix.com>
2014-05-22 01:04:26 +0100

    up version

---
+++ Readme-artirix.txt
@@ -0,0 +1,8 @@
+Readme-artirix.txt
+
+
+to create a gem:
+
+bundle exec rake release_gem[browsercms-artirix,xx]
+
+version gets read from lib/cms/version.rb
--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = "4.0.0.beta-art3"
+  VERSION = "4.0.0.beta-art4"
 
   # Return the current version of the CMS.
   def self.version
